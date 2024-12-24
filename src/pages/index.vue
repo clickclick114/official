0df6eed8e51302c34afb3d4e1a79c78409c89b64
lillian0423 <lillians423@gmail.com>
2024-12-25 07:56:37 +0800

    交換影片生物順序

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,15 +5,14 @@
       <v-main style="background: linear-gradient(to bottom, #ffffff, #b1dcf9); height: auto; position: relative;">
         <v-container class="d-flex justify-center align-center" style="height: 100%; width: 100%;">
           <v-sheet :height="'95%'" :width="'95%'" color="transparent">
-            <!-- VideoContainer 元件 -->
-            <VideoContainer />
-
             <!-- Hide on mobile screens (xs and sm breakpoints) -->
             <Character class="d-none d-md-block" />
 
             <!-- Show only on mobile screens (xs and sm breakpoints) -->
             <CharacterPhone class="d-block d-md-none" />
-
+            
+            <!-- VideoContainer 元件 -->
+            <VideoContainer />
             <!-- Section1 元件 -->
             <Section1 />
             <!-- Form 元件 -->
