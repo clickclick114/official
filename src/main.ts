7f9b40b20a1a1006173f74bc90a29224888f1123
WJY1204 <105407570+WJY1204@users.noreply.github.com>
2024-12-25 00:43:38 +0800

    壓圖片

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,20 @@ import App from './App.vue'
 // Composables
 import { createApp } from 'vue'
 
+// Import lazy loader
+import VueLazyload from 'vue-lazyload'
+
 const app = createApp(App)
 
 registerPlugins(app)
+
+// Use lazy loader
+app.use(VueLazyload, {
+    preLoad: 1.3,
+    error: '@/src/assets/error-image-photo-icon.png', // Path to error image
+    loading: '@/src/assets/loading.gif', // Path to loading image
+    attempt: 1
+})
 
 app.use(Tres)
 app.mount('#app')
